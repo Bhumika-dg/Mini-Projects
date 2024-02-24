1a65dc11a13875a1cfbfea9c9ce541e72113dbac
Bhumika Dg <157725904+Bhumika-dg@users.noreply.github.com>
2024-02-24 10:56:02 +0530

    Add files via upload

--- Project 5/hangman.py
+++ Project 5/hangman.py
@@ -18,20 +18,20 @@ def hangman():
     # print(word_letters)
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
-
+    lives = len(word_letters) + 5
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0 :
         # letter used
         
         # print(len(word_letters))
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have ',lives,' lives left and you have used these letters: ', ' '.join(used_letters))
 
         # what they have gussed till now
         word_list = [letter.upper() if letter.upper() in used_letters else '-' for letter in word]
         print('Current word: ',' '.join(word_list))
 
         user_letter = input("Guess a letter : ").upper()
-        count = count + 1
+        
         # print(f'user letter is,{user_letter}')
         if user_letter in alphabet - used_letters: # if user guessed letter is in the alphabet and not in the used_letter
             used_letters.add(user_letter) # add the guessed letter to the used_letter
@@ -39,14 +39,21 @@ def hangman():
             if user_letter in word_letters :
                 word_letters.remove(user_letter) # removing the gussed letter if it is present in the letter of the word to be guessed
                 # print(word_letters)
+                count = count + 1
+            else:
+                lives = lives - 1
+                print("Letter not in the word")
         elif user_letter in used_letters :
             print("You have already used that character. Please try again ")
             
         else:
             print("Enter a valid character. ")
 
-    word_list = [letter.upper() if letter.upper() in used_letters else '-' for letter in word]
-    print(f'You guessed the word correctly with {count} guesses : ',' '.join(word_list))
+    if lives == 0 :
+        print(f'Sorry you lost, the word was {word.upper()}')
+    else:    
+        word_list = [letter.upper() if letter.upper() in used_letters else '-' for letter in word]
+        print(f'You guessed the word correctly with {count} guesses : ',' '.join(word_list))
 
 
 
