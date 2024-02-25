14d4f87f3c2f0691580e9aed090a9677e3a4f37f
Bhumika Dg <157725904+Bhumika-dg@users.noreply.github.com>
2024-02-25 20:35:49 +0530

    Update transform.py

--- Project 11/transform.py
+++ Project 11/transform.py
@@ -1,15 +1,3 @@
-"""
-Python Image Manipulation by Kylie Ying (modified from MIT 6.865)
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 from image import Image
 import numpy as np
 
@@ -42,7 +30,7 @@ def adjust_contrast(image, factor, mid):
     return new_im
 
 def blur(image, kernel_size):
-    # kernel size is the number of pixels to take into account when applying the blur
+  
     # (ie kernel_size = 3 would be neighbors to the left/right, top/bottom, and diagonals)
     # kernel size should always be an *odd* number
     x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
@@ -61,12 +49,7 @@ def blur(image, kernel_size):
     return new_im
 
 def apply_kernel(image, kernel):
-    # the kernel should be a 2D array that represents the kernel we'll use!
-    # for the sake of simiplicity of this implementation, let's assume that the kernel is SQUARE
-    # for example the sobel x kernel (detecting horizontal edges) is as follows:
-    # [1 0 -1]
-    # [2 0 -2]
-    # [1 0 -1]
+  
     x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
     new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
     neighbor_range = kernel.shape[0] // 2  # this is a variable that tells us how many neighbors we actually look at (ie for a 3x3 kernel, this value should be 1)
@@ -84,8 +67,7 @@ def apply_kernel(image, kernel):
     return new_im
 
 def combine_images(image1, image2):
-    # let's combine two images using the squared sum of squares: value = sqrt(value_1**2, value_2**2)
-    # size of image1 and image2 MUST be the same
+    
     x_pixels, y_pixels, num_channels = image1.array.shape  # represents x, y pixels of image, # channels (R, G, B)
     new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
     for x in range(x_pixels):
@@ -99,31 +81,31 @@ if __name__ == '__main__':
 
     city = Image(filename='city.png')
 
-    # brightening
+   
     brightened_im = brighten(lake, 1.7)
     brightened_im.write_image('brightened.png')
 
-    # darkening
+   
     darkened_im = brighten(lake, 0.3)
     darkened_im.write_image('darkened.png')
 
-    # increase contrast
+    
     incr_contrast = adjust_contrast(lake, 2, 0.5)
     incr_contrast.write_image('increased_contrast.png')
 
-    # decrease contrast
+   
     decr_contrast = adjust_contrast(lake, 0.5, 0.5)
     decr_contrast.write_image('decreased_contrast.png')
 
-    # blur using kernel 3
+    
     blur_3 = blur(city, 3)
     blur_3.write_image('blur_k3.png')
 
-    # blur using kernel size of 15
+    
     blur_15 = blur(city, 15)
     blur_15.write_image('blur_k15.png')
 
-    # let's apply a sobel edge detection kernel on the x and y axis
+   
     sobel_x = apply_kernel(city, np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
     sobel_x.write_image('edge_x.png')
     sobel_y = apply_kernel(city, np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]]))
